fec95a902656c84990914aba83a4c4167bb049ad
Ademola-giod <idowuademola65@gmail.com>
2025-09-13 18:08:50 +0100

    added speedinsight

--- client/login-page/src/main.jsx
+++ client/login-page/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppContextProvider } from './context/AppContextProvider.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 createRoot(document.getElementById('root')).render(
